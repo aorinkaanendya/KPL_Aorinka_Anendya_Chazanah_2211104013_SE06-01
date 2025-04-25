fc2e4e5455cba09ef1599d7acdd14397e2d57717
aorinkaanendya <aorinkanendya@gmail.com>
2025-04-25 15:51:30 +0700

    Menambahkan  tugas jurnal modul 10 library construction

---
+++ 10_Library_Construction/JURNAL/MatematikaLibraries/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+
+namespace MathLibrary
+{
+    public class MatematikaLibraries
+    {
+        // A. Mencari Faktor Persekutuan Terbesar
+        public static int FPB(int input1, int input2)
+        {
+            while (input2 != 0)
+            {
+                int temp = input2;
+                input2 = input1 % input2;
+                input1 = temp;
+            }
+            return input1;
+        }
+
+        // B. Mencari Kelipatan Persekutuan Terkecil
+        public static int KPK(int input1, int input2)
+        {
+            return (input1 * input2) / FPB(input1, input2);
+        }
+
+        // C. Mendapatkan hasil turunan dari persamaan sederhana
+        public static string Turunan(int[] persamaan)
+        {
+            StringBuilder result = new StringBuilder();
+            int degree = persamaan.Length - 1;
+
+            for (int i = 0; i < persamaan.Length - 1; i++)
+            {
+                int coef = persamaan[i] * (degree - i);
+                if (coef == 0) continue;
+
+                if (result.Length > 0)
+                    result.Append(coef > 0 ? " + " : " - ");
+                else if (coef < 0)
+                    result.Append("-");
+
+                coef = Math.Abs(coef);
+                int power = degree - i - 1;
+
+                if (power == 0)
+                    result.Append(coef);
+                else if (power == 1)
+                    result.Append($"{coef}x");
+                else
+                    result.Append($"{coef}x{power}");
+            }
+
+            return result.ToString();
+        }
+
+        // D. Mendapatkan hasil integral dari persamaan sederhana
+        public static string Integral(int[] persamaan)
+        {
+            StringBuilder result = new StringBuilder();
+            int degree = persamaan.Length;
+
+            for (int i = 0; i < persamaan.Length; i++)
+            {
+                double coef = (double)persamaan[i] / (degree - i);
+                if (coef == 0) continue;
+
+                if (result.Length > 0)
+                    result.Append(coef > 0 ? " + " : " - ");
+                else if (coef < 0)
+                    result.Append("-");
+
+                coef = Math.Abs(coef);
+                int power = degree - i;
+
+                if (coef == 1)
+                    result.Append($"x{power}");
+                else
+                    result.Append($"{coef}x{power}");
+            }
+
+            result.Append(" + C");
+            return result.ToString();
+        }
+    }
+}
---
+++ 10_Library_Construction/JURNAL/MatematikaProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MathLibrary; 
+
+namespace MatematikaConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Program Matematika ===\n");
+
+            // A. FPB
+            int fpb = MatematikaLibraries.FPB(60, 45);
+            Console.WriteLine($"FPB dari 60 dan 45 adalah: {fpb}");
+
+            // B. KPK
+            int kpk = MatematikaLibraries.KPK(12, 8);
+            Console.WriteLine($"KPK dari 12 dan 8 adalah: {kpk}");
+
+            // C. Turunan
+            int[] persamaan1 = { 1, 4, -12, 9 };
+            string turunan = MatematikaLibraries.Turunan(persamaan1);
+            Console.WriteLine($"Turunan dari x^3 + 4x^2 - 12x + 9 adalah: {turunan}");
+
+            // D. Integral
+            int[] persamaan2 = { 4, 6, -12, 9 };
+            string integral = MatematikaLibraries.Integral(persamaan2);
+            Console.WriteLine($"Integral dari 4x^3 + 6x^2 - 12x + 9 adalah: {integral}");
+
+            Console.WriteLine("\nProgram selesai. Tekan sembarang tombol untuk keluar...");
+            Console.ReadKey();
+        }
+    }
+}
